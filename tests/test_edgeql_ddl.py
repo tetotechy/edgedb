846deaf5b223798f045246b95959385518de339d
Michael J. Sullivan <sully@msully.net>
2023-06-06 23:54:50 -0700

    Make the test case use the actual vector.edgeql source

--- tests/test_edgeql_ddl.py
+++ tests/test_edgeql_ddl.py
@@ -15824,7 +15824,7 @@ class TestDDLNonIsolated(tb.DDLTestCase):
 
     async def _extension_test_03(self):
         await self.con.execute('''
-            create extension vector;
+            create extension test_vector;
 
             create scalar type v3 extending vector::vector<3>;
         ''')
@@ -16038,117 +16038,24 @@ class TestDDLNonIsolated(tb.DDLTestCase):
             }
         ''')
 
+    # To enable a rapid test cycle, load the vector
+    # module from source under a different name
+    # and test against it.
     async def test_edgeql_ddl_extensions_03(self):
-        # XXX: This will eventually go into a file in `ext` directory.
-
-        # await self.con.execute('''
-        #     create extension package vector version '1.0' {
-        #         set ext_module := "vector";
-        #         set sql_extensions := ["vector"];
-
-        #         create module vector;
-
-        #         create scalar type vector::vector extending std::anyscalar {
-        #             set sql_type := "vector";
-        #             set sql_type_scheme := "vector({__arg_0__})";
-        #             set num_params := 1;
-        #         };
-
-        #         create cast from vector::vector to std::str {
-        #             set volatility := 'Immutable';
-        #             using sql cast;
-        #         };
-
-        #         create cast from std::str to vector::vector {
-        #             set volatility := 'Immutable';
-        #             using sql cast;
-        #         };
-
-        #         create cast from vector::vector to std::json {
-        #             set volatility := 'Immutable';
-        #             using sql 'SELECT val::text::jsonb';
-        #         };
-
-        #         create cast from std::json to vector::vector {
-        #             set volatility := 'Immutable';
-        #             using sql $$
-        #             SELECT (
-        #                 CASE WHEN nullif(val, 'null'::jsonb) IS NULL THEN NULL
-        #                 ELSE
-        #                     (SELECT COALESCE(array_agg(j), ARRAY[]::jsonb[])
-        #                     FROM jsonb_array_elements(val) as j)
-        #                 END
-        #             )::float[]::vector
-        #             $$;
-        #         };
-
-        #         create function vector::euclidean_distance(
-        #             a: vector::vector,
-        #             b: vector::vector,
-        #         ) -> std::float64 {
-        #             using sql 'SELECT a <-> b';
-        #         };
-
-        #         create function vector::inner_product(
-        #             a: vector::vector,
-        #             b: vector::vector,
-        #         ) -> std::float64 {
-        #             using sql 'SELECT -(a <#> b)';
-        #         };
-
-        #         create function vector::cosine_distance(
-        #             a: vector::vector,
-        #             b: vector::vector,
-        #         ) -> std::float64 {
-        #             using sql 'SELECT a <=> b';
-        #         };
-
-        #         create function vector::len(a: vector::vector) -> std::int64 {
-        #             using sql function 'vector_dims';
-        #             set force_return_cast := true;
-        #         };
-
-        #         create function vector::euclidean_norm(
-        #             a: vector::vector
-        #         ) -> std::float64 {
-        #             using sql function 'vector_norm';
-        #             set force_return_cast := true;
-        #         };
-
-        #         create function vector::mean(
-        #             a: set of vector::vector
-        #         ) -> vector::vector {
-        #             using sql function 'avg';
-        #             set force_return_cast := true;
-        #         };
-
-        #         create abstract index vector::ivfflat_euclidean(
-        #             named only lists: int64
-        #         ) {
-        #             create annotation std::description :=
-        #                 'IVFFlat index for euclidean distance.';
-        #         };
-
-        #         create abstract index vector::ivfflat_ip(
-        #             named only lists: int64
-        #         ) {
-        #             create annotation std::description :=
-        #                 'IVFFlat index for inner product.';
-        #         };
-
-        #         create abstract index vector::ivfflat_cosine(
-        #             named only lists: int64
-        #         ) {
-        #             create annotation std::description :=
-        #                 'IVFFlat index for cosine distance.';
-        #         };
-        #     };
-        # ''')
         try:
+            root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            with open(os.path.join(root, 'edb/lib/ext/vector.edgeql')) as f:
+                contents = f.read()
+
+            contents = contents.replace(
+                'extension package vector',
+                'extension package test_vector',
+            )
+            await self.con.execute(contents)
+
             async with self._run_and_rollback():
                 await self._extension_test_03()
         finally:
-            pass
-            # await self.con.execute('''
-            #     drop extension package vector version '1.0'
-            # ''')
+            await self.con.execute('''
+                drop extension package test_vector version '1.0'
+            ''')
